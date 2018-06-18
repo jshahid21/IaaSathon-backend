2b2312ba9819fd10d2ead49afd07610b95f65865
schmidtp0740 <schmidtp0740@gmail.com>
2018-06-18 12:15:08 -0500

    added db connection commands

--- main.go
+++ main.go
@@ -1,4 +1,25 @@
 package main
 
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
 func main() {
+	connectionString := "oracle://"
+	connectionString += os.Getenv("ORACLE_USERNAME") + ":"
+	connectionString += os.Getenv("ORACLE_PASSWD") + "@"
+	connectionString += os.Getenv("ORACLE_SID")
+	db, err := sql.Open("goracle", connectionString)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	result, err := db.Exec("SELECT * FROM *")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Print(result)
 }
