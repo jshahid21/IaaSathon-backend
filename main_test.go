eaf2efa4387ab3d983acbc9e5564b4cd0a8ffe02
schmidtp0740 <schmidtp0740@gmail.com>
2018-06-20 14:33:57 -0500

    added check functions and completed TestGetPoll test

--- main_test.go
+++ main_test.go
@@ -1,24 +1,36 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 // TestGetPolls test the GET REST API endpoint
 // for getPolls function
 func TestGetPolls(t *testing.T) {
 	// create test router
+	log.Print("testing getting polls")
 
+	request, err := http.NewRequest("GET", "/getPoll", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	response := httptest.NewRecorder()
 	// call router with path /getPolls and GET method
 
 	// verify that content-type is application/json
+	request.Header.Add("Content-type", "application/json")
 
 	// call the endpoint
+	getPolls(response, request)
 
 	// verify that response code is 200 OK
-
-	// verify that the response header
-	// content-type is application/json
+	checkResponseCode(t, http.StatusOK, response.Code)
 
 	// verify that the the response body is OK
+	checkResponseBody(t, response.Body.String(), `{"status": "OK"}`)
 }
 
 // TestSubmitPoll test the POST API endpoint
@@ -41,4 +53,20 @@ func TestSubmitPoll(t *testing.T) {
 
 	// verify that response body is a JSON body
 	// with "cat": # and "dog": #
+}
+
+// checkResponseCode verify the response code
+func checkResponseCode(t *testing.T, expected, actual int) {
+	// if response status is equal to tested status then ok
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+// checkResponseBody verify the response body
+func checkResponseBody(t *testing.T, actualBody, expectedBody string) {
+	// if response body is equal to tested body then ok
+	if actualBody != expectedBody {
+		t.Errorf("Expected %s. Got %s\n", expectedBody, actualBody)
+	}
 }
